81865c79f5bf122c8ef7c0426540c679363731e1
Bilalijaz71 <66086532+Bilalijaz71@users.noreply.github.com>
2022-03-18 05:54:42 -0700

    jmjm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import{Post} from './Post.model' 
+import { FetchService } from './file.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'httpnew';
+
+export class AppComponent implements OnInit {
+isFetching=true;
+loadedPosts:Post[]=[];
+
+  constructor(private http: HttpClient,private functionservice:FetchService) {}
+
+  ngOnInit() {
+    this.isFetching=true;
+    this.functionservice.fetchingData().subscribe(getting=>{
+this.isFetching=false;
+this.loadedPosts=getting;
+    });
+  }
+  onCreatePost(postData:Post){
+  this.functionservice.CreateandstorePost(postData.title,postData.content);
+  
+  }
+  onFetchPosts() {
+    // Send Http request
+    this.isFetching=true;
+    this.functionservice.fetchingData().subscribe(getting=>{
+      this.isFetching=false;
+      this.loadedPosts=getting;
+          });
+  }
+
+  onClearPosts() {
+    // Send Http request
+    this.functionservice.deletePost().subscribe(del=>{
+this.loadedPosts=[];
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FetchService } from './file.service';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [FetchService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
